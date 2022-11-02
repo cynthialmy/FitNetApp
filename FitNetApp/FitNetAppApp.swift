5510e2f16e5b3b2f17423848b278ba17c2b9e29d
cynthialmy <cynthiamengyuanli@gmail.com>
2022-11-02 13:07:34 -0400

    Initial Commit

---
+++ FitNetApp/FitNetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitNetAppApp.swift
+//  FitNetApp
+//
+//  Created by Cynthia LI on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FitNetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
